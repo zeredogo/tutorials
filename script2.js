e2b2fd844ccf95759348bb177ed72b70f4647d15
Paul Dogo <zeredogo@gmail.com>
2025-11-11 13:56:28 +0100

    update file

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
 let string = "KrptoZone " + "DogoEmpire";
 const stringName = `${Paul } + ${Sunday } + ${Dogo}`;
 
-let arrayProduct = ['Shoes', 'Clothe', 'Phone', 'Banana', 'Apple', 'Gra']
+let arrayProduct = ['Shoes', 'Clothe', 'Phone', 'Banana', 'Apple', 'Grap']
