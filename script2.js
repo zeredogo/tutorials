68012c1f2106676beb4bddc5ee6028986e20917a
Paul Dogo <zeredogo@gmail.com>
2025-11-27 19:20:52 +0100

    update file

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ const stringName = `${Paul } + ${Sunday } + ${Dogo}`;
 
 let arrayProduct = ['Shoes', 'Clothe', 'Phone', 'Banana', 'Apple', 'Grape', 5790, true];
 
-let objectPer = {
+let objectPers = {
    fullName: 'Paul Sunday Dogo',
    location: 'Opp. minning junction Tudun/Wada, Jos.'
 }
