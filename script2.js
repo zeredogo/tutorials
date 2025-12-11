04686462c5ab82f8a669e4cbc96f6f6a25e4308a
Paul Dogo <zeredogo@gmail.com>
2025-12-11 23:01:25 +0100

    update file

--- script2.js
+++ script2.js
@@ -6,7 +6,7 @@ let arrayProduct = ['Shoes', 'Clothe', 'Phone', 'Banana', 'Apple', 'Grape', 5790
 let objectPerson = {
    fullName: {
       firstName: 'Paul',
-      middleNa,
+      middleNam,
       lastName: 'Sunday'
    },
    location: ['Plateau', 'Jos', 'Opp. minning junction Tudun/Wada, Jos.']
