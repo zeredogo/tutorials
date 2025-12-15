6b7745191f0d160ff127fd9627848cb7553c365e
Paul Dogo <zeredogo@gmail.com>
2025-12-15 23:10:50 +0100

    update file

--- script2.js
+++ script2.js
@@ -13,4 +13,4 @@ let objectPerson = {
 }
 
 console.log(stringName, string, arrayProduct);
- console.log(objectPerson.fullName.firstName, objectPerson.loc)
+ console.log(objectPerson.fullName.firstName, objectPerson.loca)
