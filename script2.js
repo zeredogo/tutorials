f029d7c339387000ea5bb17eb01cbc8d450f8d69
Paul Dogo <zeredogo@gmail.com>
2025-12-17 21:19:01 +0100

    update file

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ let objectPerson = {
       firstName: 'Paul',
       middleNam: 'Dogo',
       lastName: 'Sunday'
-      do
+      dob
    },
    location: ['Plateau', 'Jos', 'Opp. minning junction Tudun/Wada, Jos.']
 }
