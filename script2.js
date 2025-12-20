c91151a0b73476f9f2dfdd1dbf22de8f0f2d0afc
Paul Dogo <zeredogo@gmail.com>
2025-12-20 19:21:10 +0100

    update file

--- script2.js
+++ script2.js
@@ -11,7 +11,7 @@ let objectPerson = {
       dob: {
          day: '15',
          month: '11',
-         ye
+         yea
       }
    },
    location: ['Plateau', 'Jos', 'Opp. minning junction Tudun/Wada, Jos.']
