f59feac25fb6181214bf3f7e85d8dc20145fc200
Paul Dogo <zeredogo@gmail.com>
2025-12-28 00:50:26 +0100

    update file

--- script2.js
+++ script2.js
@@ -13,7 +13,7 @@ let objectPerson = {
          month: '11',
          year: '1995',
       },
-      color: ['Red', 'Black', 'Gray', 'Yellow', 'Blue', 'Green', 'Marroon','Gold', '']
+      color: ['Red', 'Black', 'Gray', 'Yellow', 'Blue', 'Green', 'Marroon','Gold', 'W']
    },
    location: ['Plateau', 'Jos', 'Opp. minning junction Tudun/Wada, Jos.']
 }
